a7839f82382d02c4124dbf81068f848805c32c66
rayd25051992 <rayd_250592@outlook.com>
2014-07-28 03:23:17 +0100

    App submitted 280714

---
+++ app/src/main/java/com/magic8ball/raymond/magic8ball/mainactivity.java
@@ -0,0 +1,97 @@
+package com.magic8ball.raymond.magic8ball;
+import java.util.Random;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.EditText;
+
+
+
+
+
+public class mainactivity extends Activity {
+
+
+    String[] respones = { "Yes", "No", "Check Again", "I'm not sure", "Ask again",
+            "Better luck next time", "OK", "Sure thing", "Sureeee", "Maybe", "OK", "Totally" };
+
+    int length = respones.length;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mainactivity);
+       final EditText q = (EditText)findViewById(R.id.question);
+        final TextView answer = (TextView)findViewById(R.id.ans);
+
+        Button button = (Button)findViewById(R.id.buttonask);
+
+        button.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View v) {
+
+
+
+
+                        String quest = q.getText().toString();
+
+                        if (quest == "")
+                        {
+answer.setText("You must ask me a question first");
+
+                        }
+
+                        else if (quest == "Please ask a question here")
+
+                        {
+
+                            answer.setText("You must ask me a question first");
+
+                        }
+
+                        else
+
+                        {
+
+                            int idx = new Random().nextInt(respones.length);
+                            String random = (respones[idx]);
+
+
+                            answer.setText(random);
+
+
+                        }
+
+                    }
+                }
+        );
+     
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.mainactivity, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
+
